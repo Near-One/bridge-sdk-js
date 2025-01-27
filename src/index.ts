29bf2fa9d8a8f15bf9b5e77d1d1b0511aa01a79f
r-near <163825889+r-near@users.noreply.github.com>
2025-01-27 00:21:08 -0800

    Export classes/types in index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Export main client and factory
+export * from "./client"
+export * from "./factory"
+
+// Export API types and utilities
+export * from "./api"
+export * from "./utils"
+
+// Export type definitions
+export * from "./types"
+
+// Export specific clients
+export * from "./clients"
