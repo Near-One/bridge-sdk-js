aa01a49f14f0f1b807360525f7796871cd87eb87
r-near <163825889+r-near@users.noreply.github.com>
2025-01-28 19:25:29 -0800

    Add variable for mainnet/testnet

--- src/proofs/wormhole.ts
+++ src/proofs/wormhole.ts
@@ -2,8 +2,8 @@ import { serialize, wormhole } from "@wormhole-foundation/sdk"
 import evm from "@wormhole-foundation/sdk/evm"
 import solana from "@wormhole-foundation/sdk/solana"
 
-export async function getVaaForTransfer(txHash: string) {
-  const wh = await wormhole("Mainnet", [evm, solana])
+export async function getVaa(txHash: string, network: "Mainnet" | "Testnet" | "Devnet") {
+  const wh = await wormhole(network, [evm, solana])
   const result = await wh.getVaa(txHash, "Uint8Array", 60_000)
   if (!result) {
     throw new Error("No VAA found")
