9fbdcb016f33eaf48ea2578ece3a66895b409e18
r-near <163825889+r-near@users.noreply.github.com>
2025-02-05 08:34:10 -0800

    Add type definitions (#59)
    * Add type definitions
    
    * Update getClient types

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,21 @@
 import { AnchorProvider as SolWallet } from "@coral-xyz/anchor"
+import type { WalletSelector } from "@near-wallet-selector/core"
 import { Wallet as EthWallet } from "ethers"
 import { Account as NearAccount } from "near-api-js"
 import { EvmBridgeClient } from "./clients/evm"
 import { NearBridgeClient } from "./clients/near"
+import { NearWalletSelectorBridgeClient } from "./clients/near-wallet-selector"
 import { SolanaBridgeClient } from "./clients/solana"
 import { ChainKind, type InitTransferEvent, type OmniTransferMessage } from "./types"
 
-type Client = EvmBridgeClient | NearBridgeClient | SolanaBridgeClient
+type Client =
+  | EvmBridgeClient
+  | NearBridgeClient
+  | NearWalletSelectorBridgeClient
+  | SolanaBridgeClient
 
 export async function omniTransfer(
-  wallet: EthWallet | NearAccount | SolWallet,
+  wallet: EthWallet | NearAccount | WalletSelector | SolWallet,
   transfer: OmniTransferMessage,
 ): Promise<string | InitTransferEvent> {
   let client: Client | null = null
@@ -19,6 +25,8 @@ export async function omniTransfer(
     client = new EvmBridgeClient(wallet, ChainKind.Eth)
   } else if (wallet instanceof SolWallet) {
     client = new SolanaBridgeClient(wallet)
+  } else {
+    client = new NearWalletSelectorBridgeClient(wallet)
   }
 
   if (!client) {
