b1d98501b9b4da98ae2887cc39cd92011531befe
r-near <163825889+r-near@users.noreply.github.com>
2025-02-11 18:50:08 -0800

    test(config): update testnet Near address in tests and snapshots (#89)

--- src/config.test.ts
+++ src/config.test.ts
@@ -10,7 +10,7 @@ describe("Config", () => {
 
   it("should set the network to testnet", () => {
     setNetwork("testnet")
-    expect(addresses.near).toBe("omni-locker.testnet")
+    expect(addresses.near).toBe("omni.n-bridge.testnet")
   })
 
   it("should set the network to mainnet", () => {
@@ -28,11 +28,11 @@ describe("Config", () => {
 
   it("should return the correct addresses for testnet", () => {
     setNetwork("testnet")
-    expect(addresses.arb).toBe("0xd025b38762B4A4E36F0Cde483b86CB13ea00D989")
-    expect(addresses.base).toBe("0x0C981337fFe39a555d3A40dbb32f21aD0eF33FFA")
-    expect(addresses.eth).toBe("0x3701B9859Dbb9a4333A3dd933ab18e9011ddf2C8")
-    expect(addresses.near).toBe("omni-locker.testnet")
-    expect(addresses.sol.locker).toBe("Gy1XPwYZURfBzHiGAxnw3SYC33SfqsEpGSS5zeBge28p")
+    expect(addresses.arb).toBe("0x0C981337fFe39a555d3A40dbb32f21aD0eF33FFA")
+    expect(addresses.base).toBe("0xa56b860017152cD296ad723E8409Abd6e5D86d4d")
+    expect(addresses.eth).toBe("0x68a86e0Ea5B1d39F385c1326e4d493526dFe4401")
+    expect(addresses.near).toBe("omni.n-bridge.testnet")
+    expect(addresses.sol.locker).toBe("862HdJV59Vp83PbcubUnvuXc4EAXP8CDDs6LTxFpunTe")
   })
 
   it("should set the base URL for OmniBridgeAPI", () => {
