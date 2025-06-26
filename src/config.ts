6eb945e4956ffe17d2e853ca6474faf859c5ff8e
r-near <163825889+r-near@users.noreply.github.com>
2025-06-26 12:11:18 -0700

    fix: ETH address on mainnet (#147)

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ const ADDRESSES = {
   mainnet: {
     arb: "0xd025b38762B4A4E36F0Cde483b86CB13ea00D989",
     base: "0xd025b38762B4A4E36F0Cde483b86CB13ea00D989",
-    eth: "0x3701B9859Dbb9a4333A3dd933ab18e9011ddf2C8",
+    eth: "0xe00c629afaccb0510995a2b95560e446a24c85b9",
     near: "omni.bridge.near",
     sol: {
       locker: "dahPEoZGXfyV58JqqH85okdHmpN8U2q8owgPUXSCPxe",
