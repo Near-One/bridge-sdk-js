1ba06b2bf681d5ceb48a17e2bc093b51a8e9e6fc
lampamazaza <igklmp212@gmail.com>
2026-07-24 22:49:10 +0400

    fix: missing testnet tokens

--- packages/core/src/utils/token.ts
+++ packages/core/src/utils/token.ts
@@ -33,6 +33,8 @@ const KNOWN_BRIDGE_TOKENS: Record<string, ChainKind> = {
   "pol.omnidep.testnet": ChainKind.Pol,
   "abs.omnidep.testnet": ChainKind.Abs,
   "strk.omnidep.testnet": ChainKind.Strk,
+  "fogo.omnidep.near": ChainKind.Fogo,
+  "aptos.omnidep.near": ChainKind.Aptos,
 }
 
 /**
